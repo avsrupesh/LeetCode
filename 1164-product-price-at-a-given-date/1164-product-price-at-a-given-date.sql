c005a30fb11ed894a4abd5c51094680dc460f8de
Venkata Siva Rupesh Akurati <115027879+avsrupesh@users.noreply.github.com>
2023-10-29 17:23:10 -0500

    Time: 836 ms (71.98%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,4 +1,12 @@
 
+select distinct a.product_id, coalesce(b.new_price,10) as price from Products a
+left join 
+(select *, rank() over(partition by product_id order by change_date DESC) as rnk
+from Products
+where change_date<='2019-08-16') as b
+on a.product_id = b.product_id and rnk=1
+
+/* second solution
 WITH t1 AS
 (SELECT DISTINCT product_id, MAX(change_date) over (PARTITION BY product_id) last_day
 FROM Products
@@ -13,3 +21,4 @@ union
 select product_id, 10 as price
 from Products 
 where product_id not in (select product_id from t1)
+*/
